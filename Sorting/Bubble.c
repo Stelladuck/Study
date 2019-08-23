46c8c3fa92eeb54270a7e11e243f1aa79e5bf192
Stelladuck <scott430@daum.net>
2019-08-23 19:50:54 +0200

    Modify Bubble.c

--- Sorting/Bubble.c
+++ Sorting/Bubble.c
@@ -3,7 +3,7 @@
 
 void main() {
 	// Declarations and definitions
-	int *array = malloc(sizeof(int) * 10);
+	int *array = malloc( sizeof(int) * 10 );
 	int n, i, j, swap;
 
 	printf( "Enter number of elements : \t");	scanf( "%d", &n );
@@ -12,15 +12,16 @@ void main() {
 	for ( i = 0; i < n; i++ ) { scanf( "%d", array + i ); }
 
 	/* 예를 들어 array에 1, 6, 8, 2, 3의 숫자들이 저장되어 있다 하자. 그러면 오름차순 버블정렬은 아래와 같다.
-		1. [1 6] 8 2 3 ···>> 1 [6 8] 2 3 ···>> 1 6 [8 2] 3 ···>> 1 6 2 [8 3] ∴ 1 6 2 3 "8" (비교 n - 1 번 시행)
-		2. [1 6] 2 3 8 ···>> 1 [6 2] 3 8 ···>> 1 2 [6 3] 8 ∴ 1 2 3 "6" 8 (비교 3번 시행)
-		3. [1 2] 3 6 8 ···>> 1 [2 3] 6 8 ∴ 1 2 "3" 6 8 (비교 2번 시행)
-		4. [1 2] 3 6 8 ∴ 1 "2" 3 6 8 (비교 1번 시행) */
+	1. [1 6] 8 2 3 ···>> 1 [6 8] 2 3 ···>> 1 6 [8 2] 3 ···>> 1 6 2 [8 3] ∴ 1 6 2 3 "8" (비교 n - 1 번 시행)
+	2. [1 6] 2 3 8 ···>> 1 [6 2] 3 8 ···>> 1 2 [6 3] 8 ∴ 1 2 3 "6" 8 (비교 3번 시행)
+	3. [1 2] 3 6 8 ···>> 1 [2 3] 6 8 ∴ 1 2 "3" 6 8 (비교 2번 시행)
+	4. [1 2] 3 6 8 ∴ 1 "2" 3 6 8 (비교 1번 시행) */
 
 	for ( i = n - 1; i > 0; i-- ) {
 		for ( j = 0; j < i; j++ ) {
-			if ( *(array + j) > *(array + j + 1) ) { // Ascending order is used
-				swap					= *(array + j);
+			if ( *(array + j) > *(array + j + 1) ) {
+				// Ascending order is used
+				swap			= *(array + j);
 				*(array + j) 		= *(array + j + 1);
 				*(array + j + 1)	= swap;
 			}
